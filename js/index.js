88f9a5001c38ac625be384ec55c1bc9a03b0d2f0
insekkei <insekkei@yahoo.com>
2015-04-24 11:26:59 +0800

    ..

--- js/index.js
+++ js/index.js
@@ -116,8 +116,6 @@ $(function(e){
         e.preventDefault();
     });
 
-
-    
     // 图片上下滚动
 	var count = $("#imageMenu li").length - 5; /* 显示 6 个 li标签内容 */
 	var interval = $("#imageMenu li:first").width();
